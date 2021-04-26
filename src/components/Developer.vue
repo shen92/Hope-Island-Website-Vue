05e4768e02fdd5f91f997123e18ad5de09cb527c
Yingjie Shen <shen92@wisc.edu>
2021-04-25 23:14:16 -0500

    add team members

--- src/components/Developer.vue
+++ src/components/Developer.vue
@@ -1,17 +1,59 @@
 <template>
-  <div class="developer">
-    hello
+  <div class="developer" :style="{flexDirection: this.$props.isReversed ? 'row' : 'row-reverse'}">
+    <div class="descriptionContainer">
+      <slot></slot>
+    </div>
+    <div class="avatarContainer">
+      <div class="avatar" :style="{backgroundImage:`url('${avatarPath}')`}"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Developer'
+  name: 'Developer',
+  props: {
+    isReversed: Boolean,
+    avatarPath: String
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .developer {
-    border: 1px solid red
+    margin: auto;
+    padding: 1vh 1vw;
+    width: 75%;
+    height: 50vh;
+    display: flex;
+    transition:  box-shadow .25s ease-out;
+    box-shadow: 8px 8px 16px #e0e0e0, -8px -8px 16px #ffffff;
+    border-radius: 10px;
+  }
+
+  .developer:hover{ 
+    box-shadow: 8px 8px 16px #bababa, -8px -8px 16px #ffffff;
+    transition:  box-shadow .25s ease-in;
+  }
+
+  .avatarContainer {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .avatar {
+    height: 100%;
+    width: 80%;
+    background-size: contain;
+    background-position: center;
+  }
+
+  .descriptionContainer {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,6 +1,108 @@
 <template>
   <div class="team">
-    <Developer/>
+    <div style="height: 60px"></div>
+    <div class="mainContent">
+      <Developer :isReversed="false" avatarPath="/images/avatars/yingjie.jpg">
+        <div class="personalInfo">
+          <div class="title">
+            Yingjie Shen 
+            <a href="https://www.linkedin.com/in/yingjie-shen-a814a916b/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="subtitle">
+            Level Designer / Engineer
+            <br>
+            UI Designer
+            <br>
+            Gameplay Designer
+            <br>
+            Web Developer
+          </div>
+        </div>
+      </Developer>
+      <Developer :isReversed="true" avatarPath="/images/avatars/yunxuan.jpg">
+        <div class="personalInfo">
+          <div class="title">
+            Yunxuan Cai 
+            <a href="https://www.linkedin.com/in/yunxuan-cai/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            </div>
+          <div class="subtitle">
+            Character Designer (Player) 
+            <br>
+            Character Engineer (Player) 
+            <br>
+            Technical Artist
+            <br>
+            Video Editor
+          </div>
+        </div>
+      </Developer>
+      <Developer :isReversed="false" avatarPath="/images/avatars/ruiqi.jpg">
+        <div class="personalInfo">
+          <div class="title">
+            Ruiqi Liu 
+            <a href="https://www.linkedin.com/in/ruiqi-liu-530833149/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="subtitle">
+            Lead Gameplay Engineer
+          </div>
+        </div>
+      </Developer>
+      <Developer :isReversed="true" avatarPath="/images/avatars/xinhe.jpg">
+        <div class="personalInfo">
+          <div class="title">
+            Xinhe Yang 
+            <a href="https://www.linkedin.com/in/xinhe-yang-92878917a/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="subtitle">
+            Character Designer (Enemy) 
+            <br>
+            Character Engineer (Enemy)
+            <br>
+            UI Engineer
+          </div>
+        </div>
+      </Developer>
+      <Developer :isReversed="false">
+        <div class="personalInfo">
+          <div class="title">
+            Peng Dan 
+            <a href="https://www.linkedin.com/in/dan-peng-4a8a191a1/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="subtitle">
+            Character Designer (Boss)
+            <br>
+            Character Engineer (Boss) 
+            <br>
+            UI Engineer
+          </div>
+        </div>
+      </Developer>
+      <Developer :isReversed="true" avatarPath="/images/avatars/jialuo.jpg">
+        <div class="personalInfo">
+          <div class="title">
+            Jialuo Zhang  
+            <a href="https://www.linkedin.com/in/jialuo-zhang-50a4b1195/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="subtitle">
+            UI Engineer
+            <br>
+            Audio Editor
+          </div>
+        </div>
+      </Developer>
+    </div>
     <Footer/>
   </div>
 </template>
@@ -20,7 +122,38 @@ export default {
 <style lang="scss" scoped>
   .team {
     height: 100%;
+    
     display: flex;
     flex-direction: column;
+  }
+
+  .mainContent {
+    margin: 5vh 0px;
+    display: flex;
+    flex-direction: column;
+    gap: 6vh;
+  }
+
+  .personalInfo {
+    width: fit-content;
+    margin-left: 8vw;
+  }
+
+  .personalInfo .title {
+    font-size: 40px;
+    display: flex;
+    align-items: center;
+  }
+
+  .personalInfo .subtitle {
+    margin-top: 5vh;
+    font-size: 20px;
+  }
+
+  i {
+    margin-left: 30px;
+    font-size: 30px;
+    color: #0072b1;
+    cursor: pointer;
   }
 </style>
